a791b2aad6a6cd5ee244b9d1c497993f80c93d92
Adrian Tache <contact@adriantache.com>
2024-04-23 17:30:07 +0200

    Try using AppDirs for storage to fix mac issue

--- composeApp/src/desktopMain/kotlin/platformSpecific/DataStorePreferences.desktop.kt
+++ composeApp/src/desktopMain/kotlin/platformSpecific/DataStorePreferences.desktop.kt
@@ -2,9 +2,16 @@ package platformSpecific
 
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
+import net.harawata.appdirs.AppDirs
+import net.harawata.appdirs.AppDirsFactory
 
 actual fun dataStorePreferences(): DataStore<Preferences> {
+    val appDirs: AppDirs = AppDirsFactory.getInstance()
+    val path = appDirs.getUserDataDir("GPTAssistant", null, null)
+
+    println(path + SETTINGS_PREFERENCES)
+
     return createDataStoreWithDefaults(
-        path = SETTINGS_PREFERENCES,
+        path = path + SETTINGS_PREFERENCES,
     )
 }
