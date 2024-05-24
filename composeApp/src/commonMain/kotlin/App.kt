20987d09d929fa756419a66d171bcf5bf794e3ae
Adrian Tache <contact@adriantache.com>
2024-05-24 11:53:12 +0200

    Revert to old app until new code is ready

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -5,9 +5,9 @@ import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import migration.MigrationProcessor
-import new_structure.presentation.NewApp
 import org.jetbrains.compose.ui.tooling.preview.Preview
 import theme.AppColor
+import view.MainScreen
 
 @Composable
 @Preview
@@ -18,8 +18,8 @@ fun App() {
                 modifier = Modifier.fillMaxSize(),
                 color = AppColor.background(),
             ) {
-//                MainScreen()
-                NewApp()  // used for testing until the new structure is complete
+                MainScreen()
+//                NewApp()  // used for testing until the new structure is complete
             }
         }
     }
