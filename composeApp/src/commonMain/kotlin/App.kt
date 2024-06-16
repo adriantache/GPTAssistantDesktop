4d54729945f16775e331f2648bf4ffd46e587f1c
Adrian Tache <contact@adriantache.com>
2024-06-16 21:12:38 +0100

    Fix import

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -1,13 +1,12 @@
-
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import migration.MigrationProcessor
-import new_structure.presentation.NewApp
 import org.jetbrains.compose.ui.tooling.preview.Preview
 import theme.AppColor
+import view.MainScreen
 
 @Composable
 @Preview
