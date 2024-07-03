3b1b6bd9854344fc03514f0ffab7e99558406005
Adrian Tache <contact@adriantache.com>
2024-07-03 21:50:37 +0100

    Bump version

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -2,8 +2,8 @@ import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
-val version = "1.0.25"
-val versionNumber = 1025
+val version = "1.0.26"
+val versionNumber = 1026
 
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
