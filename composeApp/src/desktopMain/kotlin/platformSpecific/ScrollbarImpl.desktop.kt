0b11208501c0cff178c2cb31e3c58da68c02b105
Adrian Tache <contact@adriantache.com>
2024-10-14 08:24:26 +0100

    Fix deprecation

--- composeApp/src/desktopMain/kotlin/platformSpecific/ScrollbarImpl.desktop.kt
+++ composeApp/src/desktopMain/kotlin/platformSpecific/ScrollbarImpl.desktop.kt
@@ -1,11 +1,11 @@
 package platformSpecific
 
-import androidx.compose.foundation.ScrollbarAdapter
 import androidx.compose.foundation.VerticalScrollbar
 import androidx.compose.foundation.lazy.LazyListState
+import androidx.compose.foundation.rememberScrollbarAdapter
 import androidx.compose.runtime.Composable
 
 @Composable
 actual fun ScrollbarImpl(state: LazyListState) {
-    VerticalScrollbar(ScrollbarAdapter(state))
+    VerticalScrollbar(rememberScrollbarAdapter(state))
 }
