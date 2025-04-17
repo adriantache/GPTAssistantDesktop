59125f5a24447b94840a669847eae198c3a5363c
Adrian Tache <contact@adriantache.com>
2025-04-17 17:29:32 +0100

    Bump version

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -2,7 +2,7 @@
 import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
-val version = "1.0.33"
+val version = "1.0.34"
 val versionNumber = getVersionInt()
 
 plugins {
